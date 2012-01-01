5bc9e7a46dc41b9a2194a1c9fbcd33ae5a3709f0
fatiherikli <fatiherikli@localhost>
2012-01-02 01:12:38 +0200

    fixed footer

--- static/js/script.js
+++ static/js/script.js
@@ -3,5 +3,7 @@
         $("#id_start_date, #id_end_date").datepicker({"dateFormat":'yy-mm-dd 23:59'} );
         $("#id_title").slugify("#id_slot");
         //todo: these functions should be at add_slot.html and add_advertisement.html
+
+
     })
 
